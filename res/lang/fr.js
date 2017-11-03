97098fbff73b75cedab347491410278ef66222ec
Robin Jullian <38robin38@gmail.com>
2017-11-03 12:34:35 +0100

    Delete fr.js

--- res/lang/fr.js
+++
@@ -1,7 +0,0 @@
-var lang = {
-	noUpdate:"Pas de mise a jour disponible",
-	UpdateCompleted:"Mise a jour effectué",
-	EnterUrl:"Entrez l'URL d'un site Web",
-	addButton:"Ajouter",
-	RemoveButton:"Annuler"
-};
