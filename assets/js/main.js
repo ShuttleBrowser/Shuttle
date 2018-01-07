b7e91532251138464f25a9e010f085fb97dbbfa1
Vahelnir <vavaballz@gmail.com>
2018-01-07 20:06:54 +0100

    Refactoring with standard

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,86 +1,87 @@
-const webview = document.querySelector('webview');
-const bar = document.querySelector('.bar');
-const path = __dirname+'/assets/data.json';
-const {shell} = require('electron');
+const webview = document.querySelector('webview')
+const bar = document.querySelector('.bar')
+const path = __dirname + '/assets/data.json'
+const { shell } = require('electron')
 const $ = jQuery;
 
 //Remove the mouswheel click
-(function() {
-  function callback(e) {
-    var e = window.e || e;
+(function () {
+  function callback (e) {
+    var e = window.e || e
     if (e.target.localName == 'a') {
-      e.preventDefault();
+      e.preventDefault()
     }
     return
   }
 
   if (document.addEventListener) {
-    document.addEventListener('auxclick', callback, false);
+    document.addEventListener('auxclick', callback, false)
   } else {
-    document.attachEvent('onauxclick', callback);
+    document.attachEvent('onauxclick', callback)
   }
 })()
 
 //function to open the url
-function showWebsite(url) {
-  if (url.indexOf("http://") == 0 || url.indexOf("https://") == 0) {
-    webview.setAttribute('src', url);
-  } else if (url.indexOf("http://") == -1) {
-    webview.setAttribute('src', "http://"+url);
+function showWebsite (url) {
+  if (url.indexOf('http://') == 0 || url.indexOf('https://') == 0) {
+    webview.setAttribute('src', url)
+  } else if (url.indexOf('http://') == -1) {
+    webview.setAttribute('src', 'http://' + url)
   } else {
-    console.log("Error at loading");
+    console.log('Error at loading')
   }
 }
 
 //Add horizontall scroll for navbar
-function scrollHorizontally(e) {
-    e = window.event || e;
-    var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
-    document.documentElement.scrollLeft -= (delta*40); // Multiplied by 40
-    document.body.scrollLeft -= (delta*40); // Multiplied by 40
-    e.preventDefault();
+function scrollHorizontally (e) {
+  e = window.event || e
+  var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)))
+  document.documentElement.scrollLeft -= (delta * 40) // Multiplied by 40
+  document.body.scrollLeft -= (delta * 40) // Multiplied by 40
+  e.preventDefault()
 }
-bar.addEventListener("mousewheel", scrollHorizontally, false);
+
+bar.addEventListener('mousewheel', scrollHorizontally, false)
 
 //Function for add website
-function addWebsite() {
+function addWebsite () {
   vex.dialog.open({
-      message: 'Enter the url of a website:',
-      input: [
-          '<input name="URL" type="text" placeholder="http://" required />',
-      ].join(''),
-      buttons: [
-          $.extend({}, vex.dialog.buttons.YES, { text: 'Add' }),
-          $.extend({}, vex.dialog.buttons.NO, { text: 'Cancel' })
-      ],
-      callback: function (data) {
-          if (data) {
-              console.log('URL', data.URL)
+    message: 'Enter the url of a website:',
+    input: [
+      '<input name="URL" type="text" placeholder="http://" required />',
+    ].join(''),
+    buttons: [
+      $.extend({}, vex.dialog.buttons.YES, { text: 'Add' }),
+      $.extend({}, vex.dialog.buttons.NO, { text: 'Cancel' })
+    ],
+    callback: function (data) {
+      if (data) {
+        console.log('URL', data.URL)
         var save = fs.createWriteStream(path, {
           flags: 'a'
         })
-        save.write(',{"web":"'+data.URL+'"}');
-              save.end()
-             loadJSON(function(response) {
-                var link = JSON.parse(response+"]");
-                var RandomID =  "new"+Math.floor((Math.random() * 10000) + 1);
+        save.write(',{"web":"' + data.URL + '"}')
+        save.end()
+        loadJSON(function (response) {
+          var link = JSON.parse(response + ']')
+          var RandomID = 'new' + Math.floor((Math.random() * 10000) + 1)
 
-                document.querySelector('.add-btn').remove();
+          document.querySelector('.add-btn').remove()
 
-                  $("<a href=\"#\" id=\""+RandomID+"\" class=\"btn\" url=\""+data.URL+"\" onclick='showWebsite(\""+data.URL+"\")'></a>").appendTo(bar);
-                  getColor(data.URL, RandomID);
-                  $("<a href=\"javascript:addWebsite()\" class=\"add-btn\"></a>").appendTo(bar);
+          $('<a href="#" id=\"' + RandomID + '\" class="btn" url=\"' + data.URL + '\" onclick=\'showWebsite(\"' + data.URL + '\")\'></a>').appendTo(bar)
+          getColor(data.URL, RandomID)
+          $('<a href="javascript:addWebsite()" class="add-btn"></a>').appendTo(bar)
 
-                rightClick();
-             }, path);
-          }
+          rightClick()
+        }, path)
       }
-  });
+    }
+  })
 }
 
-  //on définie les régles css pour la webview
-  webview.addEventListener('dom-ready', function () {
-      webview.insertCSS('\n \
+//on définie les régles css pour la webview
+webview.addEventListener('dom-ready', function () {
+  webview.insertCSS('\n \
   ::-webkit-scrollbar {\n \
     width: 5px;\n \
     height: 5px;\n \
@@ -96,90 +97,90 @@ function addWebsite() {
   ::-webkit-scrollbar-thumb:window-inactive {\n \
     background: grey;\n \
   }')
-  });
+})
 
 
- loadJSON(function(response) {
-    var link = JSON.parse(response+"]");
-    console.log(link);
-    updateBar(link);
- }, path);
+loadJSON(function (response) {
+  var link = JSON.parse(response + ']')
+  console.log(link)
+  updateBar(link)
+}, path)
 
- function loadJSON(callback, jsonfile) {   
-    var xobj = new XMLHttpRequest();
-        xobj.overrideMimeType("application/json");
-    xobj.open('GET', jsonfile, true);
-    xobj.onreadystatechange = function () {
-          if (xobj.readyState == 4 && xobj.status == "200") {
-            callback(xobj.responseText);
-          }
-    };
-    xobj.send(null);  
- }
+function loadJSON (callback, jsonfile) {
+  var xobj = new XMLHttpRequest()
+  xobj.overrideMimeType('application/json')
+  xobj.open('GET', jsonfile, true)
+  xobj.onreadystatechange = function () {
+    if (xobj.readyState == 4 && xobj.status == '200') {
+      callback(xobj.responseText)
+    }
+  }
+  xobj.send(null)
+}
 
-function getColor(link, key) {
-  getColors('https://www.google.com/s2/favicons?domain='+link).then(colors => {
-    console.log('https://www.google.com/s2/favicons?domain='+link);
-    document.getElementById(key).style.backgroundColor = colors[4].hex();
+function getColor (link, key) {
+  getColors('https://www.google.com/s2/favicons?domain=' + link).then(colors => {
+    console.log('https://www.google.com/s2/favicons?domain=' + link)
+    document.getElementById(key).style.backgroundColor = colors[4].hex()
   })
 }
 
 //on affiche les boutons
-function updateBar(link) {
-  bar.innerHTML = "";
+function updateBar (link) {
+  bar.innerHTML = ''
   for (var key in link) {
-   $("<a href=\"#\" url=\""+link[key].web+"\" title=\""+link[key].web+"\" class=\"btn\" id=\""+key+"\" onclick='showWebsite(\""+link[key].web+"\")'></a>").appendTo(bar);
-    getColor(link[key].web, key);
+    $('<a href="#" url=\"' + link[key].web + '\" title=\"' + link[key].web + '\" class="btn" id=\"' + key + '\" onclick=\'showWebsite(\"' + link[key].web + '\")\'></a>').appendTo(bar)
+    getColor(link[key].web, key)
   }
-  $("<a href=\"javascript:addWebsite()\" class=\"add-btn\"></a>").appendTo(bar);
+  $('<a href="javascript:addWebsite()" class="add-btn"></a>').appendTo(bar)
   rightClick()
 }
 
 //Remove
-function rightClick() {
-    $('.btn').on("contextmenu", function (e) {
-      if (this.id == "0") {
-        console.warn("Nope");
-      } else {
-        var id = this.id;
-        console.log(id);
+function rightClick () {
+  $('.btn').on('contextmenu', function (e) {
+    if (this.id == '0') {
+      console.warn('Nope')
+    } else {
+      var id = this.id
+      console.log(id)
 
-        var UrlToRemove = $("#"+id).attr("url");
-        console.log(UrlToRemove);
-        removeWebsite(id, UrlToRemove);
-      }
-    });
-}
-
-function removeWebsite(ida, url) {
-  vex.dialog.confirm({
-      message: "Remove "+url+" from bookmarks?",
-      buttons: [
-        $.extend({}, vex.dialog.buttons.YES, {
-          text: 'Continue'
-        }),
-        $.extend({}, vex.dialog.buttons.NO, {
-          text: 'Cancel'
-        })
-      ],
-      callback: function (value) {
-        var id = ida;
-          if (value) {
-            var ButtonToRemove = document.getElementById(id);
-            ButtonToRemove.remove();
-            replace({
-              regex: ',{"web":"'+url+'"}',
-              replacement: "",
-              paths: [''+path+''],
-              recursive: true,
-              silent: true,
-            });
-
-          }
-      }
+      var UrlToRemove = $('#' + id).attr('url')
+      console.log(UrlToRemove)
+      removeWebsite(id, UrlToRemove)
+    }
   })
 }
 
-$(document).ready(function() {
-  $('.btn').tooltipster();
-});
+function removeWebsite (ida, url) {
+  vex.dialog.confirm({
+    message: 'Remove ' + url + ' from bookmarks?',
+    buttons: [
+      $.extend({}, vex.dialog.buttons.YES, {
+        text: 'Continue'
+      }),
+      $.extend({}, vex.dialog.buttons.NO, {
+        text: 'Cancel'
+      })
+    ],
+    callback: function (value) {
+      var id = ida
+      if (value) {
+        var ButtonToRemove = document.getElementById(id)
+        ButtonToRemove.remove()
+        replace({
+          regex: ',{"web":"' + url + '"}',
+          replacement: '',
+          paths: ['' + path + ''],
+          recursive: true,
+          silent: true,
+        })
+
+      }
+    }
+  })
+}
+
+$(document).ready(function () {
+  $('.btn').tooltipster()
+})
--- main.js
+++ main.js
@@ -1,140 +1,140 @@
 //1.3.2
-const electron = require('electron');
-const menubar = require('menubar');
-const url = require('url');
-const fs = require('fs');
-const AutoLaunch = require('auto-launch');
-const updater = require('./Updater/index.js');
-const path = require("path");
-const settings = require("electron-settings");
-const {ipcMain} = require('electron');
-const osLocale = require('os-locale');
+const electron = require('electron')
+const menubar = require('menubar')
+const url = require('url')
+const fs = require('fs')
+const AutoLaunch = require('auto-launch')
+const updater = require('./Updater/index.js')
+const path = require('path')
+const settings = require('electron-settings')
+const { ipcMain } = require('electron')
+const osLocale = require('os-locale')
 
-const BrowserWindow = electron.BrowserWindow;
-const Tray = electron.Tray;
+const BrowserWindow = electron.BrowserWindow
+const Tray = electron.Tray
 
-let settingsWin;
-let Overlay;
+let settingsWin
+let Overlay
 
-let iconPath;
+let iconPath
 
 if (settings.get('DevMod') == true) {
-	require('electron-debug')({enabled: true});
+  require('electron-debug')({ enabled: true })
 }
 
-if(require('electron-squirrel-startup')) return;
+if (require('electron-squirrel-startup')) return
 
-if (osLocale.sync().indexOf("fr_FR") > -1 || osLocale.sync().indexOf("fr_BE") >-1 || osLocale.sync().indexOf("fr_CA") >-1) {
-	var lang = require("./assets/lang/fr.js");
-} else if (osLocale.sync().indexOf("en_US") > -1 || osLocale.sync().indexOf("en_EN") > -1) {
-	var lang = require("./assets/lang/en.js");
+if (osLocale.sync().indexOf('fr_FR') > -1 || osLocale.sync().indexOf('fr_BE') > -1 || osLocale.sync().indexOf('fr_CA') > -1) {
+  var lang = require('./assets/lang/fr.js')
+} else if (osLocale.sync().indexOf('en_US') > -1 || osLocale.sync().indexOf('en_EN') > -1) {
+  var lang = require('./assets/lang/en.js')
 } else {
-	var lang = require("./assets/lang/en.js");
+  var lang = require('./assets/lang/en.js')
 }
 
 if (settings.get('ShuttleAutoLauncher') == true) {
-	var ShuttleAutoLauncher = new AutoLaunch({
-	    name: 'Shuttle',
-	});
-	//ShuttleAutoLauncher.enable();
+  var ShuttleAutoLauncher = new AutoLaunch({
+    name: 'Shuttle',
+  })
+  //ShuttleAutoLauncher.enable();
 }
 
-updater.updateAndInstall();
+updater.updateAndInstall()
 
 if (process.platform == 'darwin' || process.platform == 'linux') {
-	iconPath =  __dirname + "/assets/img/icon.png";
+  iconPath = __dirname + '/assets/img/icon.png'
 } else if (process.platform == 'win32') {
-	iconPath = __dirname + "/assets/img/icon.ico";
+  iconPath = __dirname + '/assets/img/icon.ico'
 }
-console.log(iconPath);
+console.log(iconPath)
 var mb = menubar({
-	index: "file://" + __dirname + "/index.html",
-	tooltip: "Shuttle",
-	icon: iconPath,
-	width:360,
-	height:640,
-	resizable: false,
-	title: "Shuttle",
-	preloadWindow: true,
-	autoHideMenuBar: true,
-	alwaysOnTop: settings.get('SOpen'),
-	frame: false,
-  	skipTaskbar: true
-});
+  index: 'file://' + __dirname + '/index.html',
+  tooltip: 'Shuttle',
+  icon: iconPath,
+  width: 360,
+  height: 640,
+  resizable: false,
+  title: 'Shuttle',
+  preloadWindow: true,
+  autoHideMenuBar: true,
+  alwaysOnTop: settings.get('SOpen'),
+  frame: false,
+  skipTaskbar: true
+})
 
 //We create the context menu
 const contextMenu = electron.Menu.buildFromTemplate([
   {
     label: lang.menu_about,
-    click() {
-    //We open the website at about
+    click () {
+      //We open the website at about
       electron.shell.openExternal('https://getshuttle.xyz/')
     }
   },
   {
     label: lang.menu_settings,
-    click() {
-        createSettingsWindows();
-      }
+    click () {
+      createSettingsWindows()
+    }
   },
-  {type: 'separator'},
+  { type: 'separator' },
   {
     label: lang.menu_quit,
-    click() {
-      mb.app.quit();
-      console.log('stopping');
+    click () {
+      mb.app.quit()
+      console.log('stopping')
     }
   }
 
-]);
+])
 
 mb.on('ready', function () {
-  console.log('Shuttle is ready');
+  console.log('Shuttle is ready')
   if (process.platform == 'win32') {
-    mb.tray.setContextMenu(contextMenu);
+    mb.tray.setContextMenu(contextMenu)
   }
-});
+})
 
-function createSettingsWindows() {
+function createSettingsWindows () {
   settingsWin = new BrowserWindow({
-	icon:__dirname + "/assets/img/icon.ico",
+    icon: __dirname + '/assets/img/icon.ico',
     width: 300,
     height: 400,
     resizable: false,
-    title: "Settings",
+    title: 'Settings',
     preloadWindow: true,
     frame: false,
     alwaysOnTop: true
-  });
+  })
 
   settingsWin.loadURL(url.format({
     pathname: path.join(__dirname, '/settings/index.html'),
     protocol: 'file:',
     slashes: true
-  }));
+  }))
 }
 
-function createOverlay() {
-	Overlay = new BrowserWindow({
-		width: 40,
-		height: 40,
-		resizable: false,
-		title: "Overlay",
-		preloadWindow: true,
-		frame: false,
-		skipTaskbar: true,
-		show: false,
-		transparent: true,
-		x: mb.tray.getBounds().x + 130,
-		y: mb.tray.getBounds().y - 70,
-		alwaysOnTop: true
-	});
+function createOverlay () {
+  Overlay = new BrowserWindow({
+    width: 40,
+    height: 40,
+    resizable: false,
+    title: 'Overlay',
+    preloadWindow: true,
+    frame: false,
+    skipTaskbar: true,
+    show: false,
+    transparent: true,
+    x: mb.tray.getBounds().x + 130,
+    y: mb.tray.getBounds().y - 70,
+    alwaysOnTop: true
+  })
 
   Overlay.loadURL(url.format({
     pathname: path.join(__dirname, '/overlay/index.html'),
     protocol: 'file:',
     slashes: true
-  }));
+  }))
 }
 
 //right click menu for Tray
@@ -151,18 +151,18 @@ mb.on('after-create-window', function () {
 //		}
 //	})
 
-  mb.tray.setContextMenu(contextMenu);
-	mb.tray.on('right-click', () => {
-		mb.tray.popUpContextMenu(contextMenu);
-	})
-});
+  mb.tray.setContextMenu(contextMenu)
+  mb.tray.on('right-click', () => {
+    mb.tray.popUpContextMenu(contextMenu)
+  })
+})
 
-var handleStartupEvent = function() {
+var handleStartupEvent = function () {
   if (process.platform !== 'win32') {
-    return false;
+    return false
   }
 
-  var squirrelCommand = process.argv[1];
+  var squirrelCommand = process.argv[1]
   switch (squirrelCommand) {
     case '--squirrel-install':
     case '--squirrel-updated':
@@ -175,54 +175,54 @@ var handleStartupEvent = function() {
       //   explorer context menus
 
       // Always quit when done
-      app.quit();
+      app.quit()
 
-      return true;
+      return true
     case '--squirrel-uninstall':
       // Undo anything you did in the --squirrel-install and
       // --squirrel-updated handlers
 
       // Always quit when done
-      app.quit();
+      app.quit()
 
-      return true;
+      return true
     case '--squirrel-obsolete':
       // This is called on the outgoing version of your app before
       // we update to the new version - it's the opposite of
       // --squirrel-updated
-      app.quit();
-      return true;
+      app.quit()
+      return true
   }
-};
+}
 
 if (handleStartupEvent()) {
-  return;
+  return
 }
 
 ipcMain.on('SettingSetAlwaysOnTop', (event, arg) => {
-  mb.setOption('alwaysOnTop', arg);
-  mb.hideWindow();
-  console.log(arg);
+  mb.setOption('alwaysOnTop', arg)
+  mb.hideWindow()
+  console.log(arg)
 })
 
 ipcMain.on('SettingSetFrame', (event, arg) => {
-  mb.window.webContents.send('addframe' , arg);
+  mb.window.webContents.send('addframe', arg)
   if (arg == true) {
-    mb.setOption('with', 380);
+    mb.setOption('with', 380)
   }
-});
+})
 
 ipcMain.on('OpenDevTool', (event, arg) => {
-	mb.window.openDevTools();
-});
+  mb.window.openDevTools()
+})
 
 ipcMain.on('OpenReportWindow', (event, arg) => {
-	electron.shell.openExternal('mailto:support@getshuttle.xyz?subject=[BUG SHUTTLE]');
-});
+  electron.shell.openExternal('mailto:support@getshuttle.xyz?subject=[BUG SHUTTLE]')
+})
 
 
 if (settings.get('Frame') == true) {
-    mb.setOption('with', 380);
+  mb.setOption('with', 380)
 } else if (settings.get('Frame') == false) {
-    mb.setOption('with', 360);
+  mb.setOption('with', 360)
 }
