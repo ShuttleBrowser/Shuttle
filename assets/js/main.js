292a013b598c293ddf4d0044b78d267dffe094e4
Robin Jullian <38robin38@gmail.com>
2018-01-10 15:30:31 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,3 @@
-//					   Dev by Robin Jullian, Shuttle Ltd
-//
-//   		              GNU GENERAL PUBLIC LICENSE
-//  	                     Version 3, 29 June 2007
-//		
-// 		Copyright (C) 2007 Free Software Foundation, Inc. <http://fsf.org/>
-// 		Everyone is permitted to copy and distribute verbatim copies
-// 		of this license document, but changing it is not allowed.
-
 const data = [
 	{"web": "http://google.com"},
 	{"web": "http://getshuttle.xyz"}
