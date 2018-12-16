3e421e7bffd17f8d0541aec59256708f032a9dfc
TheRolf <yann.lv@live.fr>
2018-12-16 15:51:20 +0100

    Added 3 shortcuts
    Added refresh, go back and go forward shrotcuts

--- main.js
+++ main.js
@@ -104,6 +104,13 @@ app.on('ready', () => {
     mb.window.webContents.send('SHORTCUT_REFRESH_CURRENTPAGE');
   })
 
+  electronLocalshortcut.register(mb.window, 'Alt+Left', () => {
+    mb.window.webContents.send('SHORTCUT_GOBACKINHISTORY');
+  })
+
+  electronLocalshortcut.register(mb.window, 'Alt+Right', () => {
+    mb.window.webContents.send('SHORTCUT_GOFORWARDINHISTORY');
+  })
 })
 
 EventsEmitter.on('SHOW_SHUTTLE', () => {
