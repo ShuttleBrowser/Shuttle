fa78a70cda6fcc71decec4864aed7749c20dbdb0
Robin Jullian <38robin38@gmail.com>
2019-01-03 15:18:29 +0100

    adding a temporary website adapter

--- app/modules/views.js
+++ app/modules/views.js
@@ -205,6 +205,12 @@ const view = {
           background-color: #333333; 
           } 
         `)
+        if (view.getActiveView().getURL().includes('messenger.com')) {
+          view.getActiveView().setUserAgent('')
+          view.getActiveView().insertCSS(`
+          ._z4_{overflow:hidden;}._4u-c{transition:height0.3sease;}._4sp8{min-width:350px!important;}._1z8r{padding:4px0!important;float:right!important;clear:right!important;}._1z8q{display:block!important;font-size:30px!important;float:left;padding:7px5px;}._4owc._53ij{width:40px!important;height:290px!important;}._2u_d{transform:translate(5%,-50%)!important;max-width:40px;}._nd_._2u_d{transform:translate(-105%,-50%)!important;max-width:40px;}._59s7{width:360px!important;}._3-8x{margin-top:0px!important;}._39bj{display:inline-block!important;}._2swd._kmc{margin-bottom:-9px;}._5irm{display:block;flex-direction:row;margin:08px012px;position:relative;}._kmc{overflow-x:hidden;overflow-y:auto;margin-right:-8px;padding:9px9px9px0!important;}._4_j5{max-width:250px;position:fixed;background-color:white;z-index:300;opacity:0.91;}._40fu{transform:rotate(270deg)translate(2%,-131%)!important;}._nd_._2u_d{transform:translate(-100%,-110%)!important;}._2u_d{flex-direction:row-reverse;transform:translate(-100%,0)!important;}._5zvq{transform:rotate(90deg);}._2rvp{transform:rotate(90deg);}._2u_d:nth-child(2){margin:00px!important;}._2t5t{transform:rotate(90deg);}._55q{max-width:82%;}
+          `)
+        }
       })
 
       webviewToListen.addEventListener('did-finish-load', event => {
